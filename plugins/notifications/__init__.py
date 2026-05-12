fbfd807e8c3326b70fb042de0abbee8327456ef1
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-12 15:35:58 +0200

    Update __init__.py

--- plugins/notifications/__init__.py
+++ plugins/notifications/__init__.py
@@ -1,7 +1,2 @@
 from .ws_routes import router
 __all__ = ["router"]
-
-def create_plugin():
-    r = APIRouter()
-    r.include_router(notifications_router, prefix="/notifications", tags=["notifications"])
-    return r
