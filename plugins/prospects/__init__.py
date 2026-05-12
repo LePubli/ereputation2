70da16f799132742b806c92a31fba229a212ac28
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-12 15:37:17 +0200

    Update __init__.py

--- plugins/prospects/__init__.py
+++ plugins/prospects/__init__.py
@@ -1,5 +1,6 @@
 """Plugin prospects."""
 from plugins.prospects.routes import router
 from .bulk_routes import router as bulk_r
-# puis : router.include_router(bulk_r)
+
+router.include_router(bulk_r)
 __all__ = ["router"]
