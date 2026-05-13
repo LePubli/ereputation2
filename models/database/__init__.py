7e1cbe4c35bfbb5f5603c066f56d07a2b7a17b91
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-13 17:36:39 +0200

    Update __init__.py

--- models/database/__init__.py
+++ models/database/__init__.py
@@ -1,6 +1,11 @@
 """Modèles SQLAlchemy ORM."""
 from models.database.base import Base
+from models.database.abm_list import ABMList
+from models.database.activity import Activity
 from models.database.audit_log import AuditLog
+from models.database.crm_sync_config import CRMSyncConfig
+from models.database.email_sequence import EmailSequence
+from models.database.inbound_source import InboundSource
 from models.database.pipeline_stage import PipelineStage
 from models.database.plugin_state import PluginState
 from models.database.prospect import Contact, Prospect
@@ -9,8 +14,13 @@ from models.database.user import User
 
 __all__ = [
     "Base",
+    "ABMList",
+    "Activity",
     "AuditLog",
     "Contact",
+    "CRMSyncConfig",
+    "EmailSequence",
+    "InboundSource",
     "PipelineStage",
     "PluginState",
     "Prospect",
