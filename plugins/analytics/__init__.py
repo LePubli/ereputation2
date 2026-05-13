155f2d85955003284910483633a0d6f31039bf87
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-13 14:50:50 +0200

    Update __init__.py

--- plugins/analytics/__init__.py
+++ plugins/analytics/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from .routes import router as _r
 
 router = APIRouter()
-router.include_router(_r, prefix="/analytics", tags=["analytics"])
+router.include_router(_r, prefix="/api/v1/analytics", tags=["analytics"])
 __all__ = ["router"]
