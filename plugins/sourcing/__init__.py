36207ea913a7bb6f019d49d539a9c8afc395e8ef
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-13 14:50:24 +0200

    Update __init__.py

--- plugins/sourcing/__init__.py
+++ plugins/sourcing/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from .routes import router as _r
 
 router = APIRouter()
-router.include_router(_r, prefix="/sourcing", tags=["sourcing"])
+router.include_router(_r, prefix="/api/v1/sourcing", tags=["sourcing"])
 __all__ = ["router"]
