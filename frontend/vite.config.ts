f6aaf9a056d48b6ed6ce8271888c9e5b3a38a7d8
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-13 05:08:08 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
