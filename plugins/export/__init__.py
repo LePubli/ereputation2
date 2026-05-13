6177c60209e78735dc9b2bff9ed4e35ddf6acf15
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-13 14:51:14 +0200

    Update __init__.py

--- plugins/export/__init__.py
+++ plugins/export/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from .routes import router as _r
 
 router = APIRouter()
-router.include_router(_r, prefix="/export", tags=["export"])
+router.include_router(_r, prefix="/api/v1/export", tags=["export"])
 __all__ = ["router"]
